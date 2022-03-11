6d7ee6b631df490291e1be8ac0ba06af3cb17bc0
Yashashree-Chopada <yashashreechopada@gmail.com>
2022-03-11 12:02:22 +0530

    Camel-JSON Validation Example

---
+++ JSON-Spring/src/main/java/com/mycompany/MyProcessor.java
@@ -0,0 +1,13 @@
+package com.mycompany;
+
+import org.apache.camel.Exchange;
+import org.apache.camel.Processor;
+
+public class MyProcessor implements Processor
+{
+    @Override
+    public void process(Exchange exchange) throws Exception {
+        String JsonMessage = exchange.getIn().getBody(String.class);
+
+    }
+}
